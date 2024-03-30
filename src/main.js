02f51f7c646e527e1a9362dbac1aeba6e882c431
OleksijHr <hrymovskij@gmail.com>
2024-03-30 15:17:24 +0200

    createMurkup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('#task-form');
+const list = document.querySelector('#task-list');
 
 console.log(form);
 
@@ -8,5 +9,12 @@ form.addEventListener('submit', event => {
   const submitValue = event.currentTarget.elements.taskName.value.trim();
   if (!submitValue) return;
   console.dir(event.currentTarget.elements.taskName.value);
+  onSubmit(submitValue);
+
   event.currentTarget.reset();
 });
+
+function onSubmit(value) {
+  const createStr = `<li>${value}</li>`;
+  list.insertAdjacentHTML("beforeend", createStr);
+}
